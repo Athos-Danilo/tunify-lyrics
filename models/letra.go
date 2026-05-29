ebf34a8470a539f61960be0d3db851bdb119a07b
Athos Danilo <athosdanilomarques@gmail.com>
2026-05-29 12:12:18 -0300

    Comentários

--- models/letra.go
+++ models/letra.go
@@ -6,17 +6,16 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-// Letra representa o documento exato que vai morar no nosso MongoDB
 type Letra struct {
-	ID                      primitive.ObjectID `bson:"_id,omitempty"`
-	IDMusicaSpotify         string             `bson:"id_musica_spotify"`
-	NomeMusica              string             `bson:"nome_musica"`
-	NomeArtista             string             `bson:"nome_artista"`
-	Status                  string             `bson:"status"`      // PENDENTE, PROCESSANDO, CONCLUIDO, NAO_ENCONTRADA
-	TextoLetra              *string            `bson:"texto_letra"` // Ponteiro (*) porque pode ser nulo no começo
-	Sincronizada            bool               `bson:"sincronizada"`
-	FonteLetra              *string            `bson:"fonte_letra"`
-	TentativasProcessamento int                `bson:"tentativas_processamento"`
-	CriadoEm                time.Time          `bson:"criado_em"`
-	AtualizadoEm            time.Time          `bson:"atualizado_em"`
+	ID                      primitive.ObjectID `bson:"_id,omitempty"`            // ID único da letra no MongoDB
+	IDMusicaSpotify         string             `bson:"id_musica_spotify"`        // ID da música no Spotify
+	NomeMusica              string             `bson:"nome_musica"`              // Título da música
+	NomeArtista             string             `bson:"nome_artista"`             // Nome do artista
+	Status                  string             `bson:"status"`                   // Status: PENDENTE, PROCESSANDO, CONCLUIDO, NAO_ENCONTRADA
+	TextoLetra              *string            `bson:"texto_letra"`              // Conteúdo da letra (pode ser nulo inicialmente)
+	Sincronizada            bool               `bson:"sincronizada"`             // Indica se a letra foi sincronizada
+	FonteLetra              *string            `bson:"fonte_letra"`              // Origem/fonte da letra (pode ser nulo)
+	TentativasProcessamento int                `bson:"tentativas_processamento"` // Quantidade de tentativas de processamento
+	CriadoEm                time.Time          `bson:"criado_em"`                // Data e hora de criação
+	AtualizadoEm            time.Time          `bson:"atualizado_em"`            // Data e hora da última atualização
 }
